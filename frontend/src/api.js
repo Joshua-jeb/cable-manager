1032ef361bbfc342da7284f8b5d66d0c61cb3128
Joshua <joshua@gmail.com>
2025-06-19 16:20:20 +0530

    Connected to live backend via .env

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api', // adjust if your backend uses a different port
+  baseURL: process.env.REACT_APP_API_BASE_URL,
 });
+
 
 export default API;
